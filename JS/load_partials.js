9f7aa75ac05c18c5812860acd8e7ee2b8047cd41
Phellype_Dias <phellypegabryeld@gmail.com>
2025-10-22 23:40:11 -0300

    Initial commit

---
+++ JS/load_partials.js
@@ -0,0 +1,18 @@
+// Função para carregar partes do HTML
+function loadHTML(elementId, filePath) {
+    fetch(filePath)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro ao carregar ${filePath}: ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error(error));
+}
+
+// Carregando as partes do HTML da pasta 'partials'
+loadHTML("header", "/HTML/partials/_header.html");
+loadHTML("footer", "/HTML/partials/_footer.html");
